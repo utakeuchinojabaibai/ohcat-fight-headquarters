e873719c20daf434f2a65342c63f49bc6d2b7843
utakeuchinojabaibai <183874927+utakeuchinojabaibai@users.noreply.github.com>
2025-07-10 12:21:57 +0900

    Update script.js

--- ohcat fights/script.js
+++ ohcat fights/script.js
@@ -1,5 +1,6 @@
 function toumei(){
 // 要素を取得
+  var bun = 1;
 let ele = document.getElementById('a');
 // 現在の display プロパティの値を保持
 const displayOriginal = ele.style.display;
@@ -8,5 +9,14 @@ ele.style.display = 'none';
 }
 function modoru(){
 // 元に戻して表示
+  var bun = 0;
 ele.style.display = displayOriginal;
+}
+
+function bunki(){
+if(bun = 0){
+toumei();
+}else if(bun = 1){
+modoru();
+}
 }
